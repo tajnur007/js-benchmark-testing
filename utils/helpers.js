e23018a87d58ab1111fc2466dab74b8767ba84c9
Kazi Tajnur Islam <tajnur007@gmail.com>
2023-12-16 02:25:27 +0600

    Add documentations for utility helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,20 +2,26 @@
 
 const { TESTING_LIMITS, LIMIT_STRINGS } = require('./constants');
 
-//? Every function will be called until reach limit
+/**
+ * Every function will be called until reach limit.
+ * @param  {Array<Function>} functions - Array of functions to be tested.
+ */
 function runProcess(...functions) {
   coreProcess(functions);
 }
 
-//? Every function will be called only once by passing limit as param
+/**
+ * Every function will be called only once by passing limit as param.
+ * @param  {Array<Function>} functions - Array of functions to be tested.
+ */
 function runProcessOnce(...functions) {
   coreProcess(functions, true);
 }
 
 /**
- *? Handling how process will be called based on passing the
- *? isCallOnce param. This method will serve for both the 
- *? runProcess() and runProcessOnce() methods.
+ * Executes a set of functions and measures their performance under different limits.
+ * @param {Array<Function>} functions - Array of functions to be tested.
+ * @param {boolean} isCallOnce - If true, each function is called once; otherwise, called multiple times.
  */
 function coreProcess(functions, isCallOnce = false) {
   for (let func of functions) {
@@ -44,14 +50,29 @@ function coreProcess(functions, isCallOnce = false) {
   }
 }
 
+/**
+ * Runs a function once with the specified limit.
+ * @param {Function} func - The function to be executed.
+ * @param {number} limit - The limit parameter for the function.
+ */
 function runFunctionOnce(func, limit) {
   func(limit);
 }
 
+/**
+ * Runs a function multiple times with the specified limit.
+ * @param {Function} func - The function to be executed.
+ * @param {number} limit - The limit parameter for the function.
+ */
 function runFunctionMultipleTimes(func, limit) {
   while (limit--) func();
 }
 
+/**
+ * Decorate output with the given function name or only a line string
+ * @param {string} name - Name of a function.
+ * @param {boolean} isStarting - If true, function name will be show as output; otherwise, only a line will be draw.
+ */
 function decorateFunctionName(name, isStarting = true) {
   name = `---  Testing \`${name}()\` method  ---`;
 
